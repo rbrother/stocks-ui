159d4e7c8653a0c128f430ffc4772d996530add7
Robert Brotherus <robert@iki.fi>
2025-09-27 00:11:41 +0300

    Fix ticker symbols of stocks

--- src/stocks_ui/config.cljs
+++ src/stocks_ui/config.cljs
@@ -7,23 +7,23 @@
 (def finnhub-api-key "YOUR_FINNHUB_API_KEY") ; Replace with actual API key
 (def finnhub-base-url "https://finnhub.io/api/v1")
 
-;; Finnish stock symbols (HEX exchange)
+;; Finnish stock symbols (HEX exchange) that have active Yahoo Finance tickers
 (def finnish-stocks
   [{:symbol "NOKIA.HE" :name "Nokia Oyj" :sector "Technology"}
    {:symbol "NESTE.HE" :name "Neste Oyj" :sector "Energy"}
    {:symbol "SAMPO.HE" :name "Sampo Oyj" :sector "Financial Services"}
    {:symbol "UPM.HE" :name "UPM-Kymmene Oyj" :sector "Materials"}
    {:symbol "FORTUM.HE" :name "Fortum Oyj" :sector "Utilities"}
-   {:symbol "STORA.HE" :name "Stora Enso Oyj" :sector "Materials"}
-   {:symbol "KESKO.HE" :name "Kesko Oyj" :sector "Consumer Discretionary"}
-   {:symbol "METSO.HE" :name "Metso Outotec Oyj" :sector "Industrials"}
+   {:symbol "STERV.HE" :name "Stora Enso Oyj" :sector "Materials"}
+   {:symbol "KESKOB.HE" :name "Kesko Oyj" :sector "Consumer Discretionary"}
+   {:symbol "MOCORP.HE" :name "Metso Oyj" :sector "Industrials"}
    {:symbol "ELISA.HE" :name "Elisa Oyj" :sector "Communication Services"}
    {:symbol "TELIA1.HE" :name "Telia Company AB" :sector "Communication Services"}
-   {:symbol "NORDEA.HE" :name "Nordea Bank Abp" :sector "Financial Services"}
-   {:symbol "ORION.HE" :name "Orion Oyj" :sector "Healthcare"}
+   {:symbol "NDA-FI.HE" :name "Nordea Bank Abp" :sector "Financial Services"}
+   {:symbol "ORNBV.HE" :name "Orion Oyj" :sector "Healthcare"}
    {:symbol "KOJAMO.HE" :name "Kojamo Oyj" :sector "Real Estate"}
-   {:symbol "KONE.HE" :name "KONE Oyj" :sector "Industrials"}
-   {:symbol "WARTSILA.HE" :name "Wärtsilä Oyj" :sector "Industrials"}])
+   {:symbol "KNEBV.HE" :name "KONE Oyj" :sector "Industrials"}
+   {:symbol "WRT1V.HE" :name "Wärtsilä Oyj" :sector "Industrials"}])
 
 ;; Time periods for charts
 (def time-periods
